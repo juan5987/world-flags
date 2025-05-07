eb22c9c706b7aa7010ebdc157ac7bb5ac56f444c
Juan <juan.machado.5987@gmail.com>
2025-05-07 17:01:35 +0200

    wip: button component colors input

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-button',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
  @Input() buttonText: string = 'Button';
+ @Input() mainColor: string = '#363636';
+ @Input() secondaryColor: string = '#fff';
 }
