7634c2a598ae7294d77e4a83eb6cb31a31222865
Juan <juan.machado.5987@gmail.com>
2025-05-07 17:12:45 +0200

    wip: router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomepageComponent } from "./ui/features/homepage/homepage.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomepageComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   encapsulation: ViewEncapsulation.None,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './ui/features/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    loadChildren: () => import('./ui/features/homepage/homepage.component').then(mod => mod.HomepageComponent),
+  }];
