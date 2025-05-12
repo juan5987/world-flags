341c3d7415eb3df9aa2aab182782ffe40d12fc97
Juan <juan.machado.5987@gmail.com>
2025-05-12 13:32:12 +0200

    add angular material, wip login with google

--- src/app/ui/features/login/login.component.ts
+++ src/app/ui/features/login/login.component.ts
@@ -1,12 +1,31 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, ViewEncapsulation } from '@angular/core';
+import { GoogleAuthService } from '../../../data/services/google-auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+const MODULES: any[] = [
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  FormsModule,
+  ReactiveFormsModule,
+];
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MODULES],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
   standalone: true,
 })
-export class LoginComponent { }
+export class LoginComponent { 
+  private authService = inject(GoogleAuthService);
+
+  signInWithGoogle() {
+    this.authService.login();
+  }
+}
