0c06bbb85f4576067e652e9a9ed7bf58a0d9f237
Juan <juan.machado.5987@gmail.com>
2025-05-21 10:48:22 +0200

    sign up template

--- src/app/ui/features/sign-up/sign-up.component.ts
+++ src/app/ui/features/sign-up/sign-up.component.ts
@@ -4,14 +4,33 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { ButtonComponent } from '../../../shared/components/button/button.component';
 @Component({
   selector: 'app-sign-up',
-  imports: [RouterModule],
+  imports: [RouterModule, ButtonComponent],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   encapsulation: ViewEncapsulation.None,
 })
-export class SignUpComponent {}
+export class SignUpComponent {
+  protected connectionButtonStyles = {
+    'background-color': 'var(--color-dark)',
+    color: 'var(--color-light)',
+    'font-size': '1.25rem',
+    width: '100%',
+    'margin-bottom': '0',
+    border: 'none',
+    'border-radius': '.75rem',
+  };
+  protected cancelButtonStyles = {
+    'background-color': 'orange',
+    color: 'var(--color-light)',
+    'font-size': '1.25rem',
+    width: '100%',
+    'margin-bottom': '0',
+    border: 'none',
+    'border-radius': '.75rem',
+  };
+}
