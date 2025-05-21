3e14535599fbae9e0a2325af3a0705e96663fc53
Juan <juan.machado.5987@gmail.com>
2025-05-21 10:38:28 +0200

    forgot password template

--- src/app/ui/features/forgot-password/forgot-password.component.ts
+++ src/app/ui/features/forgot-password/forgot-password.component.ts
@@ -1,10 +1,37 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ButtonComponent } from "../../../shared/components/button/button.component";
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  encapsulation: ViewEncapsulation.None,
 })
-export class ForgotPasswordComponent { }
+export class ForgotPasswordComponent { 
+  protected connectionButtonStyles = {
+    'background-color': 'var(--color-dark)',
+    color: 'var(--color-light)',
+    'font-size': '1.25rem',
+    width: '100%',
+    'margin-bottom': '0',
+    border: 'none',
+    'border-radius': '.75rem',
+  };
+
+  protected cancelButtonStyles = {
+    'background-color': 'var(--primary-color)',
+    color: 'var(--color-light)',
+    'font-size': '1.25rem',
+    width: '100%',
+    'margin-bottom': '0',
+    border: 'none',
+    'border-radius': '.75rem',
+  };
+
+  protected forgotPassword(event: Event) {
+    event.preventDefault();
+  }
+}
