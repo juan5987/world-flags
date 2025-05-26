491760cb8851888d80c379daf81df02a913a0220
Juan <juan.machado.5987@gmail.com>
2025-05-26 11:50:41 +0200

    refacto button styles

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -31,54 +31,15 @@ export class ButtonComponent {
   get buttonStyles() {
     switch (this.buttonStyle) {
       case 'form-cancel':
-        return this.formCancelStyle;
+        return 'form form-cancel';
       case 'form-validate':
-        return this.formValidateStyle;
+        return 'form form-validate';
       case 'main-menu':
-        return this.mainMenuStyle;
+        return 'main-menu';
       case 'main-menu-login':
-        return this.mainMenuLoginStyle;
+        return 'main-menu main-menu-login';
       case 'form-light':
-        return this.formLightStyle;
+        return 'form form-light';
     }
   }
-
-  private formButtonStyle = {
-    width: '140px',
-    'margin-bottom': '0',
-    'border-radius': '.75rem',
-    'font-size': '1.25rem',
-    color: 'var(--color-light)',
-  };
-
-  protected formCancelStyle = {
-    ...this.formButtonStyle,
-    'background-color': 'var(--primary-color)',
-  };
-
-  protected formValidateStyle = {
-    ...this.formButtonStyle,
-    'background-color': 'var(--color-dark)',
-  };
-
-  protected formLightStyle = {
-    ...this.formButtonStyle,
-    'background-color': 'var(--color-light)',
-    color: 'var(--color-dark)',
-  };
-
-  protected mainMenuStyle = {
-    'background-color': 'var(--color-light)',
-    color: 'var(--color-dark)',
-    'font-size': '1.5rem',
-    width: '200px',
-    'margin-bottom': '1rem',
-    'border-radius': '.5em',
-  };
-
-  protected mainMenuLoginStyle = {
-    ...this.mainMenuStyle,
-    'background-color': 'var(--primary-color)',
-    color: 'var(--color-light)',
-  };
 }
